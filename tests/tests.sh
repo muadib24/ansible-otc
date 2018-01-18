1dde0e1e2ebb2360916c0bbe46d330c448776d5f
Frank Kloeker <f.kloeker@telekom.de>
2018-01-18 22:36:39 +0100

    extend test cases

--- tests/tests.sh
+++ tests/tests.sh
@@ -6,3 +6,7 @@ ansible-playbook test.yml -e "localaction=flavor"
 
 # test ecs list
 ansible-playbook test.yml -e "localaction=list"
+
+# e2e test
+# ansible-playbook create_test.yml -e "ecs_name=travis-test01"
+# ansible-playbook delete_test.yml -e "ecs_name=travis-test01"
